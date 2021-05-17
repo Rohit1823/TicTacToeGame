c130e8f9346d2874d2c6bf507e33443c9de206f3
Rohit1823 <rohitmachale1823@gmail.com>
2021-05-17 12:22:41 +0530

    [Rohit]Add: Create a Board

---
+++ TicTacToe/TicTacToe/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("TicTacToe");
+            TicTacToeGame game = new TicTacToeGame();
+            game.CreateBoard();
+        }
+    }
+}
